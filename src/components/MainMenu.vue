e40ee747fd326e5dcec766aff1da67f4e90d6286
Melendrez Amador <58566731+usuario500@users.noreply.github.com>
2023-03-24 03:15:45 +0000

    segunda prueba

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,34 +1,34 @@
 <template>
     <div>
-      <v-app-bar app color="primary" dark>
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title class="white--text">Menú Principal</v-toolbar-title>
+      <v-app-bar app color="primary" dark class="elevation-0">
+        <v-img src="https://culiacan.tecnm.mx/wp-content/themes/tecnm/images/logo-tecnm.svg" />
         <v-spacer></v-spacer>
-        <v-btn text class="white--text">Solicitudes</v-btn>
-        <v-btn text class="white--text">Cerrar Sesión</v-btn>
         <v-btn text class="white--text" @click="showSubMenu = !showSubMenu">Nueva Solicitud</v-btn>
-  
-        <v-list-item-group v-if="showSubMenu" top="0">
-          <v-list-item v-for="item in subMenuItems" :key="item.title" link>
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title class="white--text">{{ item.title }}</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-  
+        <v-toolbar-title class="white--text">Mantenimiento</v-toolbar-title>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       </v-app-bar>
+
+
   
-      <v-navigation-drawer app v-model="drawer" color="primary" dark>
+      <v-list-item-group v-if="showSubMenu" class="sub-menu">
+        <v-list-item v-for="item in subMenuItems" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title class="white--text">{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list-item-group>
+  
+      <v-navigation-drawer app v-model="drawer" color="blue darken-2"  :right="true" :left="false">
         <v-list-item>
           <v-list-item-avatar>
-            <v-img src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" />
+            <v-img src="https://culiacan.tecnm.mx/wp-content/themes/tecnm/images/logo-itc.svg" />
           </v-list-item-avatar>
-          <v-list-item-title class="title white--text">Vuetify</v-list-item-title>
+          <v-list-item-title class="title white--text">Menu</v-list-item-title>
         </v-list-item>
   
         <v-divider></v-divider>
-  
+        <v-btn text class="white--text" @click="showSubMenu = !showSubMenu">Nueva Solicitud</v-btn>
         <v-list dense>
           <v-list-item v-for="item in items" :key="item.title" link>
             <v-list-item-icon>
@@ -40,6 +40,15 @@
       </v-navigation-drawer>
     </div>
   </template>
+  
+  <style scoped>
+  .sub-menu {
+  position: absolute;
+  top: 60px;
+  right: 280px;
+  }
+
+  </style>
   
   <script>
   export default {
@@ -57,11 +66,6 @@
           { title: 'Cerrar sesión', icon: 'mdi-logout' },
         ],
       };
-    },
-    methods: {
-      toggleSubMenu() {
-        this.showSubMenu = !this.showSubMenu;
-      },
     },
   };
   </script>
